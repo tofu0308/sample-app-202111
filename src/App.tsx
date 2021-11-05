191be24298813c698d9ed13959703430c4b5ac1c
takeshi.fujii <isisaka3000@gmail.com>
2021-11-05 21:24:57 +0900

    Intersection Types

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,25 @@ const func1 = (x: number,y: number): number => {
   return x + y
 }
 
+// Intersection Types
+type PROFILE = {
+  age: number;
+  city: string;
+}
+
+type LOGIN = {
+  username: string;
+  password: string;
+}
+
+type USER = PROFILE & LOGIN
+
+const userA: USER = {
+  age: 30,
+  city: 'tokyo',
+  username: 'xxx',
+  password: 'yyy'
+}
 
 function App() {
   return (
